f64c83d8c6bafb77b80a0b3c8e5f9797534939c0
JerryZ <jerryzhu99@gmail.com>
2018-03-29 14:46:18 -0400

    Improved difficulty example.

---
+++ difficulty.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+import local_data
+
+beatmaps = {}
+try:
+    beatmaps = pd.read_pickle("data/beatmaps.pkl").dropna()
+except FileNotFoundError:
+    import sys
+    print("Fetching data from " + sys.argv[1])
+    beatmap_data = local_data.get_beatmaps(sys.argv[1])
+    data = pd.DataFrame(beatmap_data)
+    data.to_pickle("data/beatmaps.pkl")
+    beatmaps = data.dropna()
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+sc = ax.scatter(beatmaps["ApproachRate"],
+                beatmaps["HPDrainRate"],
+                beatmaps["OverallDifficulty"], c=beatmaps["CircleSize"])
+
+ax.set_xlabel('ApproachRate')
+ax.set_ylabel('HPDrainRate')
+ax.set_zlabel('OverallDifficulty')
+
+cbar = plt.colorbar(sc)
+cbar.ax.set_ylabel('CircleSize')
+
+plt.title("Difficulty")
+
+plt.show()
